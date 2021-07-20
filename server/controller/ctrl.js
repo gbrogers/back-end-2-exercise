fb10f16797319e5449b3701e6525029c30e33823
Giselle Rogers <giselle.b.rogers@gmail.com>
2021-07-20 13:03:34 -0600

    hello

--- server/controller/ctrl.js
+++ server/controller/ctrl.js
@@ -35,9 +35,8 @@ module.exports = {
     };
     console.log("in createHouse");
     houses.push(newHouse);
-    res.status(200).send(houses);
-
     rollbar.info("house added.");
+    res.status(200).send(houses);
 
     houseID++;
   },
